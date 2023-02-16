ff580815873fa5610bccb6c98bcf23e3d38bc977
이승현 <iseunghyeon@iseunghyeons-MacBook-Pro.local>
2023-02-16 18:23:07 +0900

    create react-app 으로 생성된 기본폴더에서 App.js의 화면 커스텀한 컴퍼넌트로 교체 ( 맨 처음에 Component import export할때 참고

---
+++ src/components/Template.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Template = () => {
+    return (
+        <div>
+            <div>오늘 할 일 (0)</div>
+            <div>여기는 할 일 목록이 들어갑니다.</div>
+        </div>
+    );
+};
+
+export default Template;
