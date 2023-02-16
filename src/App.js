97571ffba12073e4fcabb14d258eec818d371191
이승현 <iseunghyeon@iseunghyeons-MacBook-Pro.local>
2023-02-16 19:17:11 +0900

    Todo Template, TodoList, TodoList component 생성해서 할일목록구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {useState}from "react";
 import './App.css';
 import Template from './components/Template';
+import TodoList from './components/TodoList';
 
 const App = () => {
-  return <Template>안녕하세요</Template>;
-}
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "할일 1",
+      checked: true
+    },
+    {
+      id: 2,
+      text: "할일 2",
+      checked: false
+    },
+    {
+      id: 3,
+      text: "할일 3",
+      checked: true
+    }
+  ]);
+  return (
+    <Template>
+        <TodoList todos={todos}/>
+    </Template>
+  );
+};
 
 export default App;
--- src/components/Template.js
+++ src/components/Template.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const Template = () => {
+const Template = ({children}) => {
     return (
         <div>
             <div>오늘 할 일 (0)</div>
-            <div>여기는 할 일 목록이 들어갑니다.</div>
+            <div>{children}</div>
         </div>
     );
 };
