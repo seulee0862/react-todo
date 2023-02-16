8fed0c571cb28601650861d48345d6fbb9315dc2
이승현 <iseunghyeon@iseunghyeons-MacBook-Pro.local>
2023-02-16 19:35:44 +0900

    react-icons 설치후 이모티콘 적용

--- src/components/Template.js
+++ src/components/Template.js
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 const Template = ({children}) => {
     return (
-        <div>
-            <div>오늘 할 일 (0)</div>
+        <div className= "Template">
+            <div className="title">오늘 할 일 (0)</div>
             <div>{children}</div>
         </div>
     );
